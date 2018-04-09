aed262979a9ca5d406ce8362731436959ebd9591
NetoPedro <pedroneto_09@hotmail.com>
2018-04-09 19:06:29 +0100

    Added min log levels

--- gologger.go
+++ gologger.go
@@ -1,5 +1,7 @@
 package gologger
 
+import "strconv"
+
 const (
 	CONSOLE       string = "console"
 	FILE          string = "file"
@@ -11,6 +13,12 @@ const (
 type GoLogger struct {
 	Logger
 }
+const DEBUG = 0
+const LOG = 1
+const INFO = 2
+const WARN = 3
+const ERROR = 4
+const FATAL = 5
 
 func GetLogger(selector ...string) GoLogger {
 	if len(selector) == 0 {
@@ -19,32 +27,44 @@ func GetLogger(selector ...string) GoLogger {
 	return GoLogger{Logger{selector[0], selector[1]}}
 }
 
-func (log GoLogger) Log(message interface{}) {
-	logPrinter(LogInstance{LogType: "LOG", Body: message, LoggerInit: log.Logger})
+func (log GoLogger) Log(message interface{}, minLevel int) {
+	if minLevel <= LOG {
+		logPrinter(LogInstance{LogType: "LOG", Body: message, LoggerInit: log.Logger})
+	}
 }
 
 func (log GoLogger) Message(message interface{}) {
 	logPrinter(LogInstance{LogType: "MSG", Body: message, LoggerInit: log.Logger})
 }
 
-func (log GoLogger) Info(message interface{}) {
-	logPrinter(LogInstance{LogType: "INF", Body: message, LoggerInit: log.Logger})
+func (log GoLogger) Info(message interface{}, minLevel int) {
+	if minLevel <= INFO {
+		logPrinter(LogInstance{LogType: "INF", Body: message, LoggerInit: log.Logger})
+	}
 }
 
-func (log GoLogger) Warn(message interface{}) {
-	logPrinter(LogInstance{LogType: "WRN", Body: message, LoggerInit: log.Logger})
+func (log GoLogger) Warn(message interface{}, minLevel int) {
+	if minLevel <= WARN {
+		logPrinter(LogInstance{LogType: "WRN", Body: message, LoggerInit: log.Logger})
+	}
 }
 
-func (log GoLogger) Debug(message interface{}) {
-	logPrinter(LogInstance{LogType: "DBG", Body: message, LoggerInit: log.Logger})
+func (log GoLogger) Debug(message interface{}, minLevel int) {
+	if minLevel <= DEBUG {
+		logPrinter(LogInstance{LogType: "DBG", Body: message, LoggerInit: log.Logger})
+	}
 }
 
-func (log GoLogger) Error(message interface{}) {
-	logPrinter(LogInstance{LogType: "ERR", Body: message, LoggerInit: log.Logger})
+func (log GoLogger) Error(message interface{}, minLevel int) {
+	if minLevel <= ERROR {
+		logPrinter(LogInstance{LogType: "ERR", Body: message, LoggerInit: log.Logger})
+	}
 }
 
-func (log GoLogger) Fatal(message interface{}) {
-	logPrinter(LogInstance{LogType: "CRT", Body: message, LoggerInit: log.Logger})
+func (log GoLogger) Fatal(message interface{}, minLevel int) {
+	if minLevel <= FATAL {
+		logPrinter(LogInstance{LogType: "CRT", Body: message, LoggerInit: log.Logger})
+	}
 }
 
 func (log GoLogger) ReplaceMessage(message interface{}) {
